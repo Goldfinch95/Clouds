1a40b85e6107238f69a2da9b479ab24e0fd894f2
Goldfinch <facu_vila@hotmail.com>
2022-08-22 15:49:55 -0300

    se trabajo en la logica del input conectado a la api

--- script.js
+++ script.js
@@ -1,56 +1,38 @@
 console.log("Bienvenido a Clouds");
 
-const input = document.querySelector("#searchBar");
-const boton = document.querySelector("#send");
+const input = document.querySelector("#searchBar"); 
+const boton = document.querySelector("#send"); 
+const ciudad = document.querySelector("#city");
+const temperatura = document.querySelector("#temp");
+const icono = document.querySelector("#icon");
+const clima = document.querySelector("#main");
+const sensaciónTermica = document.querySelector("#feels_like");
+const temperaturaMaxima = document.querySelector("#temp_max");
+const temperaturaMinima = document.querySelector("#temp_min");
+const humedad = document.querySelector("#humidity");
 
-//conectar la api
 
-    const fetchClima = (ciudad) =>{
-        fetch("https://api.openweathermap.org/data/2.5/weather?q="
-        + ciudad + 
+boton.addEventListener('click', ()=> {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="
+        + input.value + 
         "&appid=ad61bf9360774cabad49c44a75fb6423&units=metric")
         .then((respuesta) => respuesta.json())
-        .then((datos) => mostrarClima(datos));
-    }
-
-//obtener los datos
-
-    const mostrarClima = (datos) => {
-        const ciudad = datos.name;
-        const temp = datos.main.temp;
-        const clima = datos.weather[0].main;
-        const icono = datos.weather[0].icon;
-        const sensaciónTermica = datos.main.feels_like;
-        const tempMax = datos.main.temp_max;
-        const tempMin = datos.main.temp_min;
-        const humedad = datos.main.humidity;
-        
-        //console.log(ciudad, temp, clima, icono, sensaciónTermica, tempMax, tempMin, humedad);
-
-        // modificar el DOM
-
-        document.querySelector("#city").innerText = ciudad;
-        document.querySelector("#temp").innerText = temp;
-        document.querySelector("#icon").src = "https://openweathermap.org/img/wn/" + icono + ".png";
-        document.querySelector("#main").innerText = clima;
-        document.querySelector("#feels_like").innerText = "sensación termica " + sensaciónTermica;
-        document.querySelector("#temp_max").innerText = "Temperatura maxima " + tempMax;
-        document.querySelector("#temp_min").innerText = "Temperatura minima " + tempMin;
-        document.querySelector("#humidity").innerText = "Humedad del " + humedad + "%";
-    }
-
-    // funcion de busqueda
-
-    const buscar = () =>{
-        input.value;
-    }
-
-    //Hacer funcionar la barra de busqueda
-
-
-   
-
-    boton.addEventListener("click", buscar())
-
-
-    fetchClima("New York");
+        .then((datos) => {
+            let name = datos['name'];
+            let temp = datos['main']['temp'];
+            let weather = datos['weather']['0']['main'];
+            let icon = datos['weather']['0']['icon']
+            let feeling = datos['main']['feels_like'];
+            let tempMax = datos['main']['temp_max'];
+            let tempMin = datos['main']['temp_min'];
+            let humidity = datos['main']['humidity'];
+            ciudad.innerHTML = name;
+            temperatura.innerHTML = `${temp}°C`;
+            clima.innerHTML = weather;
+            icono.src = "https://openweathermap.org/img/wn/" + icon + ".png";
+            sensaciónTermica.innerHTML = `Sensación Termica: ${feeling}`;
+            temperaturaMaxima.innerHTML = `Temp Max: ${tempMax}°C`;
+            temperaturaMinima.innerHTML = `Temp Min: ${tempMin}°C`;
+            humedad.innerHTML = `Humedad: ${humidity}%`;
+        }).catch(error => alert('ingresaste una ciudad equivocada'))
+})
